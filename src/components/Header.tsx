e3424d2f1caf67c80538abbff32959959ee9a2ad
apak0 <muhampak25@gmail.com>
2025-05-07 15:04:58 +0300

    profile image example photo removed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -165,13 +165,7 @@ export function Header() {
                 className="flex items-center focus:outline-none"
                 onClick={toggleUserMenu}
               >
-                {user ? (
-                  <img
-                    alt={user.name || "User Profile"}
-                    src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
-                    className="relative inline-block h-10 w-10 cursor-pointer rounded-full object-cover object-center"
-                  />
-                ) : (
+                {user && (
                   <UserCircle className="h-8 w-8 text-gray-400 hover:text-red-600" />
                 )}
               </button>
